5aef6c3c5f37d67f78a72879f15a2923d0940784
fsorodrigues <desouzaoliveirarod.f@husky.neu.edu>
2018-07-08 01:48:44 -0400

    Change to in-app DB

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 
@@ -24,13 +25,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 mysql.init_app(app)
 
 # map models
-class Messages(mysql.Model):
-    __tablename__ = 'hello'
+class Courses(mysql.Model):
+    __tablename__ = 'courserepository'
     id = mysql.Column(mysql.Integer, primary_key=True)
-    message = mysql.Column(mysql.String(255), nullable=False)
+    title = mysql.Column(mysql.String(45), nullable=False)
+    created = mysql.Column(mysql.DateTime, nullable=False)
+    modified = mysql.Column(mysql.DateTime, nullable=False)
 
     def __repr__(self):
-        return '<Result: (%s, %s) >' % (self.id, self.message)
+        return '<Result: (%s, %s, %s, %s) >' % (self.id, self.title, self.created, self.modified)
         # return dict(id=self.id, message=self.message)
 
 @app.route("/", methods=['GET'])
@@ -38,75 +41,77 @@ def hello():
     return "Hello World!!!!!!!"
 
 # READ
-@app.route('/message', methods=['GET'])
-def getMessage():
-    data = Messages.query.all() # fetch all products on the table
+@app.route('/courses', methods=['GET'])
+def getCourses():
+    data = Courses.query.all() # fetch all products on the table
 
     data_all = []
 
-    for msg in data:
-        data_all.append(dict(id=msg.id,message=msg.message)) # prepare visual data
+    for course in data:
+        data_all.append(dict(id=course.id,title=course.title,created=course.created,modified=course.modified)) # prepare visual data
 
-    return jsonify(messages=data_all)
+    return jsonify(courses=data_all)
 
 # CREATE
-@app.route('/message', methods=['POST'])
-def createMessage():
+@app.route('/course', methods=['POST'])
+def createCourse():
 
     # fetch message from the request
-    _message = request.json['message']
+    _course = request.json['title']
 
-    message = Messages(message=_message) # prepare query statement
+    course = Courses(title=_course,created=datetime.now()) # prepare query statement
 
     curr_session = mysql.session # open database session
+
     try:
-        curr_session.add(message) # add prepared statement to opened session
+        curr_session.add(course) # add prepared statement to opened session
         curr_session.commit() # commit changes
     except:
         curr_session.rollback()
         curr_session.flush() # for resetting non-commited .add()
 
-    messageId = message.id # fetch last inserted id
-    data = Messages.query.filter_by(id=messageId).first() # fetch our inserted product
+    courseId = course.id # fetch last inserted id
+    data = Courses.query.filter_by(id=courseId).first() # fetch our inserted product
 
-    result = dict(id=data.id,message=data.message) # prepare visual data
+    result = dict(id=course.id,title=course.title,created=course.created) # prepare visual data
 
-    return jsonify(session=result)
+    return jsonify(course=result)
 
 # UPDATE
-@app.route('/<int:messageId>/message', methods=['PATCH'])
-def updateMessage(messageId):
+@app.route('/<int:courseId>/course', methods=['PATCH'])
+def updateCourse(courseId):
 
-    message = request.json['message']
-    print(message)
+    _course = request.json['title']
+
     curr_session = mysql.session
 
     try:
-        msg = Messages.query.filter_by(id=messageId).first() # fetch the message do be updated
-        msg.message = message # update column info fetched from request
+        course = Courses.query.filter_by(id=courseId).first() # fetch the entry do be updated
+        course.title = _course # update column info fetched from request
+        course.modified = datetime.now() # update modified column
         curr_session.commit() # commit changes
     except:
         curr_session.rollback()
         curr_session.flush()
 
-    messageId = msg.id
-    data = Messages.query.filter_by(id=messageId).first() # fetch our updated message
+    courseId = course.id
+    course = Courses.query.filter_by(id=courseId).first() # fetch our updated message
 
-    result = dict(id=data.id,message=data.message) # prepare visual data
+    result = dict(id=course.id,message=course.message,created=course.created,modified=course.modified) # prepare visual data
 
-    return jsonify(session=result)
+    return jsonify(course=result)
 
 
 # REMOVE
-@app.route('/message/<int:messageId>', methods=['DELETE'])
-def deleteMessage(messageId):
+@app.route('/course/<int:courseId>', methods=['DELETE'])
+def deleteCourse(courseId):
 
     curr_session = mysql.session # initiate database session
 
-    Messages.query.filter_by(id=messageId).delete() # finds the message by messageId and deletes it
+    Courses.query.filter_by(id=courseId).delete() # finds the message by messageId and deletes it
     curr_session.commit() # commit changes to the database
 
-    return getMessage() # return all messages
+    return getCourses() # return all messages
 
 if __name__ == "__main__":
     app.run()
